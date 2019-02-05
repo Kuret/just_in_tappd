4754f22c62f040797e7d9b8d33ea328284b1bfca
Kuret <r.littel@outlook.com>
2019-02-05 13:20:22 +0100

    remove default phoenix css

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -48,7 +48,6 @@ module.exports = (env, options) => ({
     ],
   },
   plugins: [
-    // new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
