c67e911291c9245449a1545d16d478201d4f5881
Kuret <r.littel@outlook.com>
2019-02-08 11:08:44 +0100

    Add heroku config stuff

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,21 @@
 use Mix.Config
 
+defmodule Prod.Config do
+  def host, do: hosts() |> List.first()
+  def hosts, do: [host_url(), heroku_app_url()] |> Enum.reject(&is_nil/1)
+  def origins, do: hosts() |> Enum.map(&("https://" <> &1))
+
+  defp host_url, do: System.get_env("HOST_URL")
+
+  defp heroku_app_url do
+    case System.get_env("HEROKU_APP_NAME") do
+      nil -> nil
+      "" -> nil
+      app_name -> app_name <> ".herokuapp.com"
+    end
+  end
+end
+
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
@@ -14,8 +30,22 @@ config :just_in_tappd, JustInTappdWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :just_in_tappd, JustInTappdWeb.Endpoint,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: Prod.Config.origins(),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  load_from_system_env: true,
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  url: [scheme: "https", host: Prod.Config.host(), port: 443]
+
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :just_in_tappd, JustInTapp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
